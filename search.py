4ca4ca01f24e98e6bf4a23c009f37d2994130c0e
Ilya798 <120042252+Ilya798@users.noreply.github.com>
2023-02-04 17:50:22 +0300

    22
    22

--- search.py
+++ search.py
@@ -5,3 +5,4 @@ url = 'http://static-maps.yandex.ru/1.x/'
 headers = {'user-agent': 'yandexlyceum/1.1.1'}
 response = requests.get(url, headers=headers, params=params)
 print(response.url)
+print('222')
