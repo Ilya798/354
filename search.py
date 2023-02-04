db75f4e579e89617ce100a0100a9e469cac184ab
Admin <vysotskijiv@yandexlyceum.ru>
2023-02-04 17:46:18 +0300

    331

---
+++ search.py
@@ -0,0 +1,7 @@
+import requests
+
+params = {'list_param': ['value1', 'value2']}
+url = 'http://static-maps.yandex.ru/1.x/'
+headers = {'user-agent': 'yandexlyceum/1.1.1'}
+response = requests.get(url, headers=headers, params=params)
+print(response.url)
